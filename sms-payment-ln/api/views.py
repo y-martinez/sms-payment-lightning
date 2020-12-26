3518d24322b5e007fab33fb4c921d961d094c818
Ybrahin Martinez <ybrahimmartinez09@gmail.com>
2020-12-26 18:01:12 -0400

    Create wallet

--- sms-payment-ln/api/views.py
+++ sms-payment-ln/api/views.py
@@ -1,8 +1,9 @@
 import importlib
 
-from rest_framework import viewsets, mixins
+from rest_framework import generics, viewsets, mixins
 from rest_framework.permissions import AllowAny, IsAuthenticated, SAFE_METHODS
 from rest_framework.views import APIView
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from app.lnd_client import LndRestClient
 from app.models import Wallet
@@ -18,6 +19,26 @@ class Test(APIView):
         else:
             return Response(response)
 
-class WalletViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+
+class WalletViewSet( mixins.CreateModelMixin,viewsets.GenericViewSet):
     queryset = Wallet.objects.all()
     serializer_class = WalletSerializer
+
+    @action(methods=['post'], detail=False)
+    def newAddress(self,request):
+        response = client.newaddress()
+        if 'error' in response.keys():
+            return Response(data = {"error": response['error']}, status=response['status_code'])
+        else:
+            return Response(response)
+
+    def create(self, request, *args,**kwargs):
+        response = self.newAddress(self)
+        if response.status_code >= 400:
+            return response
+        else:
+            mutable = request.POST._mutable
+            request.POST._mutable = True
+            request.POST['address'] = response.data['address']
+            request.POST._mutable = mutable
+        return super().create(request,args,kwargs)
